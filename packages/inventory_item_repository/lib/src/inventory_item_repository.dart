d01ee8a984ef46d14014d15d3d475b833b84f33f
Magnus Freidenfelt <sabbathmagnus@gmail.com>
2026-02-16 11:10:27 +0100

    - Updated min iOS from 13 to 15

--- packages/inventory_item_repository/lib/src/inventory_item_repository.dart
+++ packages/inventory_item_repository/lib/src/inventory_item_repository.dart
@@ -1,7 +1,3 @@
-/// {@template inventory_item_repository}
-/// A Very Good Project created by Very Good CLI.
-/// {@endtemplate}
 class InventoryItemRepository {
-  /// {@macro inventory_item_repository}
   const InventoryItemRepository();
 }
