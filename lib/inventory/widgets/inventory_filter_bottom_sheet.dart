5f993df40b0e84dd1bfb4755e43da6352cd616fa
Magnus Freidenfelt <sabbathmagnus@gmail.com>
2026-03-26 16:16:13 +0100

    - Hide empty stock switch moved from filter sheet to top of inventory page.

--- lib/inventory/widgets/inventory_filter_bottom_sheet.dart
+++ lib/inventory/widgets/inventory_filter_bottom_sheet.dart
@@ -45,20 +45,7 @@ class InventoryFilterBottomSheet extends StatelessWidget {
                         ),
                       ],
                     ),
-                    Row(
-                      mainAxisAlignment: .spaceBetween,
-                      children: [
-                        Text(l10n.inventoryShowEmptyStockText),
-                        ScaleTransition(
-                          scale: const AlwaysStoppedAnimation(0.8),
-                          child: Switch(
-                            value: state.filter.quantityFilter == .inStock,
-                            onChanged: (_) =>
-                                bloc.add(const HideEmptyStockSwitchPressed()),
-                          ),
-                        ),
-                      ],
-                    ),
+
                     Text(l10n.formFieldCategoryLabelText),
                     Wrap(
                       spacing: 8,
